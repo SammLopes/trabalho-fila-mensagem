4f7cc0f05a71b2ce7192b7e1ff72c8b5f1844daa
SammLopes <sam.v.p.lopes@gmail.com>
2024-10-06 23:24:50 -0300

    REmove annotation @EnableShecheduling e alterando o AwsConfig

--- microservico2/src/main/java/br/com/microservico2/microservico2/config/AwsConfig.java
+++ microservico2/src/main/java/br/com/microservico2/microservico2/config/AwsConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 
 import software.amazon.awssdk.auth.credentials.AnonymousCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.sqs.SqsAsyncClient;
 import software.amazon.awssdk.services.sqs.SqsClient;
 import software.amazon.awssdk.services.sqs.SqsClientBuilder;
 import java.net.URI;
@@ -14,12 +15,10 @@ import java.net.URI;
 public class AwsConfig {
     
     @Bean
-    public SqsClient sqsClient() {
-        String endpoint = System.getenv("AWS_ENDPOINT");
-        return SqsClient.builder()
-                .region(Region.US_EAST_1)
-                .credentialsProvider(AnonymousCredentialsProvider.create())
-                .endpointOverride(URI.create("http://localstack:4566")) 
+    public SqsAsyncClient sqsAsyncClient() {
+        return SqsAsyncClient.builder()
+                .endpointOverride(URI.create("http://localhost:4566"))// -> config no host
+                .region(Region.SA_EAST_1)
                 .build();
     }
 }
